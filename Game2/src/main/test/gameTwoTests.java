b590742fa956a538e982619a0908b4e7ba1a3531
Jesse <Jesse@DESKTOP-ENR6249>
2016-10-25 17:28:06 -0400

    Outline what we need to test.

--- Game2/src/main/test/gameTwoTests.java
+++ Game2/src/main/test/gameTwoTests.java
@@ -6,16 +6,26 @@ import org.junit.Test;
 //Functions to test:
 /*
  * Algea Eater Model:
- *		void rotateFish() - This is not needed as the circle is stationary, the rotation is simply animation. Need to update UML.
- *		void healthUp()
- *		void healthDown()
- *		void incrEatSpeed() - This needed? Cant just be read from crab?
- *		void perish() - This should now be in view as this is strictly animation?
+ *	void rotateFish() - This is not needed as the circle is stationary, the rotation is simply animation. Need to update UML.
+ *	void healthUp()
+ *	void healthDown()
+ *	void incrEatSpeed() - This needed? Cant just be read from crab?
+ *	void perish() - This should now be in view as this is strictly animation?
  *
  *Animal Model:
  *	void healthUP()
  *	void healthDown()
- *	void pickUp()	
+ *	void pickUp()
+ *
+ *Bar Model:
+ *	void increase()
+ *	void decrease()
+ *	bool isEmpty()
+ *
+ *Water Model:
+ *	void incConcentration()
+ *	
+ *	
  */
 public class gameTwoTests {
 	
