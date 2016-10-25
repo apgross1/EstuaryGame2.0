7eb19f2a56142be8f8116e76a13ff69fb363cb8b
Jesse <Jesse@DESKTOP-ENR6249>
2016-10-25 17:09:48 -0400

    initial

---
+++ Game2/src/main/test/gameTwoTests.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+//Functions to test:
+/*
+ * Algea Eater Model:
+ *		void rotateFish()
+ *		void healthUp()
+ *		void healthDown()
+ *		void incrEatSpeed()
+ *		void perish()
+ *
+ *Animal Model:
+ *	void healthUP()
+ *	void healthDown()
+ *	void pickUp()	
+ */
+public class gameTwoTests {
+	
+	//First model
+	@Test
+	public void testrotateFish() { //Change structure of Algea eater to eaterRotation
+		algeaEater eater = new algeaEater();
+	}
+
+}
