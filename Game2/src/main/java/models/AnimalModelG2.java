72cb1acc76a8de15c8b0694bc326d4a20ba13f32
Jesse <Jesse@DESKTOP-ENR6249>
2016-10-27 18:41:55 -0400

    Fixed g2 model stubs

--- Game2/src/main/java/models/AnimalModelG2.java
+++ Game2/src/main/java/models/AnimalModelG2.java
@@ -2,7 +2,7 @@ package models;
 
 public class AnimalModelG2 extends AnimalModelAbstract {
 	
-	public AnimalModel() {
+	public AnimalModelG2() {
 		this.setSpeed(100);
 	}
 	
@@ -16,6 +16,24 @@ public class AnimalModelG2 extends AnimalModelAbstract {
 	
 	@Override
 	public void move(){
+		
+	}
+
+	@Override
+	public void healthUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void healthDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pickUp() {
+		// TODO Auto-generated method stub
 		
 	}
 }
