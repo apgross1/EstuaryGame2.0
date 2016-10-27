3c276110eac4f0a0450e8884ed849d0f151f431c
Jesse <Jesse@128.4.90.3>
2016-10-26 22:33:01 -0400

    Outlined the rest of the game 2 controller tests...

--- Game2/src/main/test/gameTwoTests.java
+++ Game2/src/main/test/gameTwoTests.java
@@ -144,5 +144,113 @@ public class gameTwoTests {
 		assertTrue("Y should be 0...", myAnimal.getLocY() == 0);
 		
 	}
+
+/*
+ * 
+ * BEGIN TEST FOR CONTROLLER
+ * 
+ */
+/*
+ * Update in UML:
+ * 	- Change touchEater and touchAlgae to one void collisionDetected(obj a, obj b)
+ * 	- Move create storm in controller to view.
+ * 	- Remove pickedUp() as nothing is ever being picked up in game 2
+ * 	- Remove duplicate createStorm()
+ * 	- If we decide to combine the gone() functions, remove them and make a int pctGone() function.
+ */
+	@Test
+	public void testcollisionDetected(){
+		//Is this function called once a collision is already detected.... or 
+		//does it listen for action event. and then update something in the model.
+		//Either way, this one function consolidates the two functions in the UML
+		//touchEater() and touchAlgae()
+		
+	}
+	
+	@Test
+	public void testcreateStorm(){
+		//This too is in the controller but I think belongs in view because its an animation?
+		//or does this actually call activate storm which triggers runoff(an animation that also affects controller for algea quantity)?
+		//can the two functions be consolidated (createStorm(), activateStorm())
+	}
+	
+	@Test
+	public void testtakeDamage(){
+		//Action or listener?
+	}
+	
+	@Test
+	public void testisDead(){
+		//Why is this in the controller? / what purpose does this serve.
+	}
+	
+	@Test
+	public void testactivateStorm(){
+		//Can this be consolidated with createStorm?
+	}
+	
+	@Test
+	public void testincLvl(){
+		//Action event when algea collides?
+		//not sure what this function does.
+	}
+	
+	@Test
+	public void testdecLvl(){
+		//Same as above, don't understand its purpose
+	}
+	
+	@Test
+	public void pctGone(){
+		//I think we should make a function that returns an integer that tells us what percent is gone 
+		//rather than having 4 functions (quartergone, halfgone, threequartergone etc.)
+		
+		//Either way, aren't these just getters?
+	}
+	
+	@Test
+	public void testbarFull(){
+		//Not sure of the purpose of this as its just a getter of sorts as it just returns a bool.
+		
+	}
+	
+	@Test
+	public void testbarEmpty(){
+		//Same as barFull()
+		
+	}
+	
+	@Test
+	public void testdecSpeed(){
+		//Is this called when a collision is detected from one of the listeners above?
+		//If so... why do we even have this, we can have the listener talk to the model directly.
+	}
+	
+	@Test
+	public void testRenderGraphics(){
+		//This is likely not testable as this communicates the picture to the view.
+	}
+	
+	@Test
+	public void testAddObj(){
+		//Is this when an object is added to the arraylist of objects?
+	}
+	
+	@Test
+	public void testRemObj(){
+		//If yes to above, I assume this is when we remove individual algeas when collisions occur.
+	}
+	
+	@Test
+	public void testEndGame(){
+		//1) Ends timer / stops loops
+		//2) Triggers end animation?
+	}
+	
+	@Test
+	public void testStartGame(){
+		//1)Intro animation call in the view?
+		//2) Start timer / loops 
+	}
 }
 
