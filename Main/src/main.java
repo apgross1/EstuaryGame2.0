f45702e228a6fe1f97309fbf8c59a063093c0c70
iiqqrs <iiqqrs@gmail.com>
2016-12-06 16:09:59 -0500

    commit for merge

--- Main/src/main.java
+++ Main/src/main.java
@@ -1,60 +1,20 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
 import javax.swing.JFrame;
 
 import controller.Game1Controller;
 import controller.Game2Controller;
 import controller.Game3Controller;
-import models.AnimalModelG3;
 
-public class main implements Serializable{
-
+public class main {
+	
 	public static void main(String[] args) {
+		JFrame gameFrame = new JFrame();
 		
-JFrame gameFrame = new JFrame();
-		
-		
-		Game1Controller g1 = new Game1Controller(gameFrame);
-		/*try {
-			FileOutputStream fileOut = new FileOutputStream("./game1.txt");
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(g1);
-			out.close();
-			fileOut.close();
-			System.out.printf("Serialized data is saved in ./game1.txt file");
-		} catch (IOException i) {
-			i.printStackTrace();
+		MainRun game = new MainRun(gameFrame);
+		while(!game.isStartPressed()){
+			game.repaintFrame();
 		}
-		Game2Controller g2 = new Game2Controller(gameFrame);
-		try {
-			FileOutputStream fileOut = new FileOutputStream("./game2.txt");
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(g2);
-			out.close();
-			fileOut.close();
-			System.out.printf("Serialized data is saved in ./game2.txt file");
-		} catch (IOException i) {
-			i.printStackTrace();
-		}
-		Game3Controller g3 = new Game3Controller(gameFrame);
-		try {
-			FileOutputStream fileOut = new FileOutputStream("./game3.txt");
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(g3);
-			out.close();
-			fileOut.close();
-			System.out.printf("Serialized data is saved in ./game3.txt file");
-		} catch (IOException i) {
-			i.printStackTrace();
-		}
-		*/
-		g1.startGame();
-		//g2.startGame();
-		//g3.runGame();
-
+		//Once the start game button is pressed well get here.
+		System.out.println("Yup, were good.");
 	}
-
 }
