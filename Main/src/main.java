73dc5cde897ac95b260009ffde3bb5122c876cbe
Daniel Kim <dankim@udel.edu>
2016-12-07 18:03:50 -0500

    commit

--- Main/src/main.java
+++ Main/src/main.java
@@ -24,7 +24,7 @@ public class main {
 
 		Game1Controller g1 = new Game1Controller(gameFrame);
 		//Game2Controller g2 = new Game2Controller(gameFrame);
-		//Game3Controller g3 = new Game3Controller(gameFrame, true);
+		Game3Controller g3 = new Game3Controller(gameFrame, true);
 		
 		g1.startGame();
 		//g2.startGame();
